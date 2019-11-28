f26ac47ea0bbbb12a1ebca8a1bcf0321fe937f6a
Ramanpreet Singh <ramanpreet230@gmail.com>
2019-11-27 19:27:19 -0500

    Changes in layout.

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -29,23 +29,23 @@ public class MainActivity extends AppCompatActivity {
         demoAdapter = new DemoAdapter(list);
 
         // For Linear Layout
-        //RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        //recyclerView.setLayoutManager(layoutManager);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true);
+        recyclerView.setLayoutManager(layoutManager);
 
         // For Grid Layout
         //GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         //recyclerView.setLayoutManager(gridLayoutManager);
 
         // For StaggeredGrid Layout
-        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        //StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        //recyclerView.setLayoutManager(staggeredGridLayoutManager);
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(demoAdapter);
-
+        prepareData();
     }
 
-        private void prepareData() {
+    private void prepareData() {
         Country india = new Country("INDIA", R.drawable.in);
         list.add(india);
         Country pak = new Country("PAKISTAN", R.drawable.pk);
